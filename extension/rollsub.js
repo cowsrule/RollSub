0b5bb6e5d36cc07e57c2893526cb87df837b458f
Grant Watters <grant.watters@gmail.com>
2016-08-22 16:07:31 -0700

    Add linter config, fix errors.

--- extension/rollsub.js
+++ extension/rollsub.js
@@ -1,58 +1,69 @@
-var frames = document.getElementsByClassName('framedata');
+'use strict';
 
-function generateQueryString(data)
+(function ()
 {
-    var queryString = '';
+    var HOST = 'https://localhost';
+    var PORT = 6027;
 
-    if (typeof data === 'object')
+    var frames = document.getElementsByClassName('framedata');
+
+    function generateQueryString(data)
     {
-        var queryArr = [ ];
+        var queryString = '';
 
-        for (var key in data)
+        if (typeof data === 'object')
         {
-            queryArr[queryArr.length] = encodeURIComponent(key) + '=' + encodeURIComponent(data[key]);
+            var queryArr = [ ];
+
+            for (var key in data)
+            {
+                if (data.hasOwnProperty(key))
+                {
+                    queryArr[queryArr.length] = encodeURIComponent(key) + '=' + encodeURIComponent(data[key]);
+                }
+            }
+
+            queryString = queryArr.join('&').replace(/%20/g, '+');
+        }
+        else
+        {
+            queryString = data;
         }
 
-        queryString = queryArr.join('&').replace(/%20/g, '+');
-    }
-    else
-    {
-        self.Assert(typeof data === 'string', 'Only Objects or Strings are currently supported');
-
-        queryString = data;
+        return queryString;
     }
 
-    return queryString;
-}
-
-for (var i = 0; i < frames.length; ++i)
-{
-    var frame = frames[i];
-
-    var loc = frame.firstElementChild;
-
-    var file = loc.children[0].innerText.slice(1, -1);
-    var line = parseInt(loc.children[1].innerText);
-    var col = parseInt(loc.children[2].innerText);
-
-    // var linkNode = document.createElement('span');
-
-    // linkNode.href = 'http://' + file + '.' + line + ':' + col;
-    // linkNode.innerText = 'GOTO';
-
-    loc.addEventListener('click', function (file, line, col)
+    function handleClick(file, line, col)
     {
         var xhr = new XMLHttpRequest();
 
         var data = { file: file, line: line, col: col };
         var dataString = generateQueryString(data);
 
-        xhr.open('POST', 'https://localhost:6027/redir?' + dataString, true);
+        xhr.open('POST', HOST + ':' + PORT + '/redir?' + dataString);
 
         xhr.send();
-    }.bind(undefined, file, line, col));
+    }
 
-    loc.style.cursor = 'pointer';
+    for (var i = 0; i < frames.length; ++i)
+    {
+        var frame = frames[i];
 
-    // loc.insertBefore(linkNode, loc.firstChild);
-}
+        try
+        {
+            var loc = frame.firstElementChild;
+
+            var fileParse = loc.children[0].innerText.slice(1, -1);
+            var lineParse = parseInt(loc.children[1].innerText, 10);
+            var colParse = parseInt(loc.children[2].innerText, 10);
+
+            loc.addEventListener('click', handleClick.bind(undefined, fileParse, lineParse, colParse));
+
+            loc.style.cursor = 'pointer';
+        }
+        catch (err)
+        {
+            // Ignore
+        }
+    }
+})();
